3b01d522a0360e0c052123344951e664124136b8
biancangming <942242856@qq.com>
2023-08-10 14:30:40 +0800

    修改为vite打包工具，便于支持vue文件的使用

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,3 @@
-import { createRequire } from 'node:module';
-
-export function require(path: string) {
-    return createRequire(import.meta.url)(path);
-}
 
 export function add(a: number, b: number) {
     return a + b
